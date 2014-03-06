fe046d4de887dd7fa8b5cb43af1925b516f23c93
Liz Abinante <eabinante@gmail.com>
2014-03-06 16:49:32 -0600

    template in case i forget how to run mocha tests

---
+++ template.js
@@ -0,0 +1,10 @@
+// run with this command:
+// mocha file.js --watch
+
+var assert = require("assert");
+
+describe('thing', function() {
+  it('does stuff', function() {
+    // assertions
+  })
+})
